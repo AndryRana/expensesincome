8c46b2e8cf909d3e691000810555ff0bbb957c1e
AndryRana <lsourcus@gmail.com>
2023-04-01 12:46:36 +0200

    CRUD+Search Income & User password reset

--- expenses/urls.py
+++ expenses/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('expensesapp.urls')),
+    path('income/', include('userincome.urls')),
     path('authentication/', include('authenticationapp.urls')),
     path('preferences/', include('userpreferences.urls')),
 ]
